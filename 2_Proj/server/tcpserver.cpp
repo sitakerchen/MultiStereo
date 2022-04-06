e2c7b555e7b7c9e1089a7b204e481e52430d8545
starkchen <sitaker@yahoo.com>
2022-04-06 15:25:51 +0800

    add send file function

--- 2_Proj/server/tcpserver.cpp
+++ 2_Proj/server/tcpserver.cpp
@@ -17,9 +17,18 @@ tcpserver::tcpserver(QMainWindow *parent)
     ui->pushButtonDisconnect->setEnabled(true);
     ui->plainTextEditRecv->setReadOnly(true);
     ui->pushButtonSend->setEnabled(false);
+    ui->pushButton_sendFile->setEnabled(false);
+    ui->pushButton_chooseFile->setEnabled(false);
     m_bIsListening = false;
 
     connect(m_tcpServer, SIGNAL(newConnection()), this, SLOT(NewConnectionSlot()));
+    connect(&m_outFile.timer,&QTimer::timeout,[=](){
+        //关闭定时器
+        m_outFile.timer.stop();
+
+        //发送文件
+        sendData();
+    });
 }
 
 tcpserver::~tcpserver()
@@ -27,21 +36,64 @@ tcpserver::~tcpserver()
     delete ui;
 }
 
+void tcpserver::sendData()
+{
+    qint64 len = 0;
+        do
+        {
+           //每次发送数据的大小 4K
+            char buf[4*1024] = {0};
+            len = 0;
+
+            //往文件中读数据
+            len  = m_outFile.file.read(buf,sizeof(buf));
+            //发送数据，读多少 ，发多少
+            len = m_currentClient->write(buf,len);
+
+            //发送的数据需要积累
+            m_outFile.sendSize += len;
+
+        }
+        while(len > 0);
+
+
+        //是否发送文件完毕
+        if(m_outFile.sendSize == m_outFile.fileSize)
+        {
+            ui->plainTextEditSend->appendPlainText("文件发送完毕");
+            m_outFile.file.close();
+
+            //把客户端关闭
+            m_currentClient->disconnectFromHost();
+            m_currentClient->close();
+        }
+}
+
 void tcpserver::ReadData()
 {
-    for (int i = 0 ; i < m_tcpClient.length() ; ++ i)
+    for (int i = 0 ; i < m_ListTcpClient.length() ; ++ i) // traver through every established connections
     {
-        QByteArray buffer = m_tcpClient[i]->readAll();
+        /*  recv data from client */
+        QByteArray buffer = m_ListTcpClient[i]->readAll();
         if (buffer.isEmpty()) continue;
 
+
         static QString IP_Port, IP_Port_Pre;
-        IP_Port = tr("[%1:%2]:").arg(m_tcpClient[i]->peerAddress().toString().split("::ffff:")[1])\
-                .arg(m_tcpClient[i]->peerPort());
+        IP_Port = tr("[%1:%2]:").arg(m_ListTcpClient[i]->peerAddress().toString().split("::ffff:")[1])\
+                .arg(m_ListTcpClient[i]->peerPort());
 
         if (IP_Port != IP_Port_Pre)
             ui->plainTextEditRecv->appendPlainText(IP_Port);
 
         ui->plainTextEditRecv->appendPlainText(buffer);
+        if (QString(buffer) == "file done") // file send successfully
+        {
+            QMessageBox::information(this, tr("File send"), tr("file send finish"));
+            m_outFile.file.close();
+            // cut off connection
+            m_currentClient->disconnectFromHost();
+            m_currentClient->close();
+        }
 
         IP_Port_Pre = IP_Port;
     }
@@ -50,9 +102,13 @@ void tcpserver::ReadData()
 void tcpserver::NewConnectionSlot()
 {
     m_currentClient = m_tcpServer->nextPendingConnection();
-    m_tcpClient.append(m_currentClient);
+    m_ListTcpClient.append(m_currentClient);
     ui->comboBox->addItem(tr("%1:%2").arg(m_currentClient->peerAddress().toString().split("::ffff:")[1]).arg(m_currentClient->peerPort()));
     ui->pushButtonSend->setEnabled(true);
+
+    /* enable only if connxion is established*/
+    ui->pushButton_chooseFile->setEnabled(true);
+    ui->pushButton_sendFile->setEnabled(true);
 
     connect(m_currentClient, SIGNAL(readyRead()), this, SLOT(ReadData()));
     connect(m_currentClient, SIGNAL(disconnected()), this, SLOT(disConnectedSlot()));
@@ -61,18 +117,18 @@ void tcpserver::NewConnectionSlot()
 void tcpserver::disConnectedSlot()
 {
     /* 由于disconnected信号没有提供请求断开连接窗口的描述信息所以需要遍历定位再断开连接并删除 */
-    for (int i = 0 ; i < m_tcpClient.length() ; ++ i)
+    for (int i = 0 ; i < m_ListTcpClient.length() ; ++ i)
     {
-        if (m_tcpClient[i]->state() == QAbstractSocket::UnconnectedState)
+        if (m_ListTcpClient[i]->state() == QAbstractSocket::UnconnectedState)
         {
             ui->comboBox->removeItem(ui->comboBox->findText(tr("%1:%2")\
-                                                            .arg(m_tcpClient[i]->peerAddress().toString().split("::ffff:")[1])\
-                                     .arg(m_tcpClient[i]->peerPort())));
-            m_tcpClient[i]->destroyed();
-            m_tcpClient.removeAt(i);
+                                                            .arg(m_ListTcpClient[i]->peerAddress().toString().split("::ffff:")[1])\
+                                     .arg(m_ListTcpClient[i]->peerPort())));
+            m_ListTcpClient[i]->destroyed();
+            m_ListTcpClient.removeAt(i);
         }
     }
-    if (m_tcpClient.empty())
+    if (m_ListTcpClient.empty())
     {
         ui->pushButtonSend->setEnabled(false);
     }
@@ -101,10 +157,10 @@ void tcpserver::on_pushButtonDisconnect_clicked()
 
     /* 关闭所有连接并停止监听窗口 */
     // 有问题，会崩溃：考虑只保留disconnectFromHost其他删掉
-    for (int i = 0 ; i < m_tcpClient.length() ; ++ i)
+    for (int i = 0 ; i < m_ListTcpClient.length() ; ++ i)
     {
-        m_tcpClient[i]->disconnectFromHost();
-        if (m_tcpClient[i]->state() == QAbstractSocket::UnconnectedState or m_tcpClient[i]->waitForDisconnected(1000))
+        m_ListTcpClient[i]->disconnectFromHost();
+        if (m_ListTcpClient[i]->state() == QAbstractSocket::UnconnectedState or m_ListTcpClient[i]->waitForDisconnected(1000))
         {
             QMessageBox::information(this, tr("Success"), tr("disconnect success"));
         }
@@ -114,7 +170,7 @@ void tcpserver::on_pushButtonDisconnect_clicked()
             QMessageBox::critical(this, tr("Error"), tr("disconnected fail"));
         }
     }
-    m_tcpClient.clear();
+    m_ListTcpClient.clear();
     m_tcpServer->close();
 
     m_bIsListening = false;
@@ -151,13 +207,79 @@ void tcpserver::on_pushButtonSend_clicked()
     qDebug() << "clienPort :" << clientPort << endl;
 
 
-    for (int i = 0 ; i < m_tcpClient.length() ; ++ i)
+    for (int i = 0 ; i < m_ListTcpClient.length() ; ++ i)
     {
-        qDebug() << m_tcpClient[i]->peerAddress().toString().split("::ffff:")[1] <<endl;
-        if ( m_tcpClient[i]->peerAddress().toString().split("::ffff:")[1] == clientIP and m_tcpClient[i]->peerPort() == clientPort)
+        qDebug() << m_ListTcpClient[i]->peerAddress().toString().split("::ffff:")[1] <<endl;
+        if ( m_ListTcpClient[i]->peerAddress().toString().split("::ffff:")[1] == clientIP and m_ListTcpClient[i]->peerPort() == clientPort)
         {
-            m_tcpClient[i]->write(qsSend.toLatin1());
+            m_ListTcpClient[i]->write(qsSend.toLatin1());
             return ;
         }
     }
+}
+
+void tcpserver::on_pushButton_chooseFile_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this,"open","../");
+
+
+        if (false == filePath.isEmpty())
+        {
+            m_outFile.fileName.clear();
+            m_outFile.fileSize = 0;
+
+            //获取文件信息
+            QFileInfo info(filePath);
+            m_outFile.fileName = info.fileName();//获取文件名字
+            m_outFile.fileSize = info.size();//获取文件大小
+
+            m_outFile.sendSize = 0;//发送文件的大小
+
+            //只读方式打开文件
+            //指定文件的名字
+            m_outFile.file.setFileName(filePath);
+
+            //打开文件
+            bool isOk = m_outFile.file.open(QIODevice::ReadOnly);
+            if (false == isOk)
+            {
+                qDebug()<<"只读方式打开文件失败 78";
+            }
+
+            //提示打开文件路径
+            ui->lineEdit_FileName->setText(filePath);
+
+            ui->pushButton_chooseFile->setEnabled(false);
+            ui->pushButton_sendFile->setEnabled(true);
+        }
+        else
+        {
+            qDebug()<<"选择文件路径出错 62 ";
+        }
+}
+
+
+void tcpserver::on_pushButton_sendFile_clicked()
+{
+    //先发送文件头信息  格式如：文件名##文件大小
+    QString head = QString("%1##%2").arg(m_outFile.fileName).arg(m_outFile.fileSize);
+
+    //发送头部的信息
+    qint64 len = m_currentClient->write(head.toUtf8());
+
+    if (len > 0)//头部信息发送成功
+    {
+        //发送真正的文件信息
+        //防止TCP黏包文件
+        //需要通过定时器延时 20ms
+        m_outFile.timer.start(20);
+    }
+    else
+    {
+        qDebug()<<"头部信息发送失败 114";
+        m_outFile.file.close();
+        ui->pushButton_chooseFile->setEnabled(true);
+        ui->pushButton_sendFile->setEnabled(false);
+    }
+
 }
--- 2_Proj/server/tcpserver.h
+++ 2_Proj/server/tcpserver.h
@@ -6,6 +6,7 @@
 #include <QTcpSocket>
 #include <QNetworkInterface>
 #include <QMessageBox>
+#include <QFileDialog>
 #include "../public/mediafile.h"
 
 QT_BEGIN_NAMESPACE
@@ -22,10 +23,13 @@ public:
 private:
     Ui::tcpserver *ui;
     QTcpServer *m_tcpServer; // tcp服务器
-    QList<QTcpSocket*> m_tcpClient; // 客户端列表
+    QList<QTcpSocket*> m_ListTcpClient; // 客户端列表
     QTcpSocket *m_currentClient; // 现在连接的客户端
     mediaFile m_outFile;
     bool m_bIsListening; // 是否处于监听开启状态
+
+private:
+    void sendData();
 
 private slots:
     // server slot function
@@ -37,6 +41,8 @@ private slots:
     void on_pushButtonDisconnect_clicked(); // 开启与关闭服务器的监听与连接
     void on_pushButtonClearWindow_clicked(); // 清除接收窗口内容
     void on_pushButtonSend_clicked(); // 发送数据到客户端
+    void on_pushButton_chooseFile_clicked();
+    void on_pushButton_sendFile_clicked();
 };
 
 #endif // TCPSERVER_H
